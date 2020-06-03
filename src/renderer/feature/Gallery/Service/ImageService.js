d822f886330e40f2f047c9210b404eb6074cfd02
Susam <susam.buildings@gmail.com>
2020-06-03 22:18:14 +0300

    add simple storage

--- src/renderer/feature/Gallery/Service/ImageService.js
+++ src/renderer/feature/Gallery/Service/ImageService.js
@@ -1,5 +1,5 @@
-import { catalogsInfo, favCatalog, allImagesInfo } from '../TestImages/.hidden';
 import * as log from 'loglevel';
+import storage from '../../Storage/Storage';
 
 const logger = log.getLogger('images-service');
 logger.setLevel(log.levels.DEBUG);
@@ -7,22 +7,26 @@ logger.setLevel(log.levels.DEBUG);
 class ImageService {
     getHomeImages() {
         logger.debug('getting home images');
-        return [...allImagesInfo];
+        return storage.getAllImages();
     }
 
     getFavoriteImages() {
         logger.debug('getting favorite images');
+        const favCatalog = storage.getFavorites();
+        const allImagesInfo = storage.getAllImages();
         const ids = favCatalog.content;
         return allImagesInfo.filter((image) => ids.includes(image.id));
     }
 
     getPhotoCollections() {
         logger.debug('getting collections');
-        return [...catalogsInfo];
+        return storage.getCollections();
     }
 
     getPhotoCollectionImages(collectionSlug) {
         logger.debug('getting content of collection', collectionSlug);
+        const catalogsInfo = storage.getCollections();
+        const allImagesInfo = storage.getAllImages();
         const catalog = catalogsInfo.filter(
             (it) => it.slug === collectionSlug,
         )[0];
---
+++ src/renderer/feature/Storage/Storage.js
@@ -0,0 +1,22 @@
+import { catalogsInfo, favCatalog, allImagesInfo } from './TestImages/.hidden';
+
+class Storage {
+    getAllImages() {
+        return [...allImagesInfo];
+    }
+
+    getFavorites() {
+        return [...favCatalog];
+    }
+
+    getCollections() {
+        return [...catalogsInfo];
+    }
+
+    setImages() {}
+    addImage() {}
+    removeImage() {}
+}
+
+const storage = new Storage();
+export default storage;
