111220c515d808f41c7998b15bb65e866a4434d2
Susam <susam.buildings@gmail.com>
2020-06-07 13:58:32 +0300

    fix upload link highlight

--- src/renderer/module/CollectionCreator/UI/Menu/CollectionCreatorMenu.jsx
+++ src/renderer/module/CollectionCreator/UI/Menu/CollectionCreatorMenu.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './CollectionCreatorMenu.module.scss';
 import * as classnames from 'classnames';
 
 function CollectionCreatorMenu({ routePrefix: base }) {
+  const location = useLocation();
+  const currentUrl = location.pathname;
+
+  console.log(currentUrl, base);
+
   return (
     <>
-      <li className={classnames(styles.menuItem, styles.upload)}>
+      <li
+        className={classnames({
+          [styles.menuItem]: true,
+          [styles.upload]: true,
+          [styles.active]: currentUrl === `${base}`,
+        })}
+      >
         <Link to={`${base}`} className={styles.menuLink}>
           Upload
         </Link>
